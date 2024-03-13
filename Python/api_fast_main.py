da342e9ba3b57e1283e6754fba481237ff5fbbf2
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-13 15:42:43 -0300

    new_secret

--- Python/api_fast_main.py
+++ Python/api_fast_main.py
@@ -2,13 +2,22 @@ from fastapi import FastAPI
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from Python.Firebase.firebase_config import caminho_secret
+from firebase_config_FASTAPI import caminho_secret
 
 app = FastAPI()
+
+def conectar_banco():
+    cred = credentials.Certificate(caminho_secret)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://books-fiction-go-default-rtdb.firebaseio.com/'
+    })
+
+@app.get("/fiction")
+def get_books_fiction():
+    conectar_banco()
+    ref = db.reference('/fiction')
+    return ref.get()
 
 @app.get("/")
 def home():
     return "Bem-vindo à API de Livros! Esta é uma API para consultar livros de ficção que estão no banco de dados do Firebase, e livros de não ficção no banco de dados MongoDB."
-
-
-#função get /books_fiction que vai ser conectado com o banco de dados do Firebase
