571ad4a069f94b3bbd62dd55a3a252bae3243828
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-13 10:08:06 -0300

    att

--- Python/solicitar.py
+++ Python/solicitar.py
@@ -6,7 +6,7 @@ response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    formatted_data = json.dumps(data, indent=4)  # Indenta o JSON para uma melhor legibilidade
+    formatted_data = json.dumps(data, indent=4)
     print(formatted_data)
 else:
     print('Erro ao fazer a solicitação:', response.status_code)
