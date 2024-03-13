e3512d95681010d27d0fce4c7f33ff6b3090d987
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-13 15:23:40 -0300

    save

--- fastapi/api_main.py
+++ fastapi/api_main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Bem-vindo à API de Livros! Esta é uma API para consultar livros de ficção que estão no banco de dados do Firebase, e livros de não ficção no banco de dados MongoDB."
