482682495cf5fbe23bf95902d44ea8363ddffa8b
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-14 09:54:24 -0300

    save

--- Python/solicitar.py
+++ Python/solicitar.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:8000/livro_de_nao_ficcao'
+url = 'http://127.0.0.1:8000/livro_de_ficcao'
 response = requests.get(url)
 
 if response.status_code == 200:
